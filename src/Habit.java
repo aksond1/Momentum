36d29e825baf7c79bebf5737a6f72210894eeba0
aksond1 <stas.kravchyk.34@gmail.com>
2026-09-11 20:34:54 +0300

    Create a completeHabit() method

--- src/Habit.java
+++ src/Habit.java
@@ -1,12 +1,16 @@
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Habit {
     private String habitName;
     private LocalDateTime createdAt;
-    // completedDates
-    // currentStreak
-    // longestStreak
+
+    private List<LocalDate> completedDates = new ArrayList<>();
+    private int currentStreak;
+    private int longestStreak;
 
     public Habit(String habitName) {
         this.habitName = habitName;
@@ -24,6 +28,30 @@ public class Habit {
         habitName = scanner.nextLine();
 
         System.out.println("Habit was successfully renamed to: " + habitName);
+    }
+
+    public void completeHabit() {
+        System.out.print("Did you do this habit today? (yeah/nah): ");
+        String habitComplete = scanner.nextLine();
+
+        if (habitComplete.equals("yeah")) {
+            completedDates.add(LocalDate.now());
+            currentStreak++;
+
+            if (currentStreak > longestStreak) {
+                longestStreak = currentStreak;
+            }
+
+            System.out.println("I'm proud of you buddy. You're a real legend today, keep it up!");
+        } else if (habitComplete.equals("nah")) {
+            System.out.println("You're kinda weak idk");
+        } else {
+            System.out.println("Please learn how to write properly");
+        }
+
+        System.out.println("Your current streak is: " + currentStreak);
+        System.out.println("Your longest streak is: " + longestStreak);
+        System.out.println("Completed dates: " + completedDates);
     }
 
     public void deleteHabit() {
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Habit habit = new Habit("Morning reading");
         habit.editHabit();
+        habit.completeHabit();
         habit.deleteHabit();
     }
 }
